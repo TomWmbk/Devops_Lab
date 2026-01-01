0018c3a01d358765d3656ea1a63e9aed073f0684
TomWmbk <tom.wambeke23@gmail.com>
2026-01-01 17:38:06 +0100

    Complete lab on Version Control, Build Systems, and Automated Testing

--- td4/scripts/sample-app/app.js
+++ td4/scripts/sample-app/app.js
@@ -1,21 +1,12 @@
-// app.js
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Exercice 7 : Endpoint paramétré
 app.get('/name/:name', (req, res) => {
     res.send(`Hello, ${req.params.name}!`);
-});
-
-// Exercice 9 : Endpoint somme
-app.get('/add/:a/:b', (req, res) => {
-    const sum = parseInt(req.params.a) + parseInt(req.params.b);
-    res.send(sum.toString());
 });
 
 module.exports = app;
